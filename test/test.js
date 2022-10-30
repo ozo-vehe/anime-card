24f2c661e9eb6670b5a846656c797d1176f5378b
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-10-30 11:57:44 +0400

    updated tests to reflect smart contract changes

--- test/test.js
+++ test/test.js
@@ -8,6 +8,8 @@ describe("AnimeCard", function () {
   let owner;
   let acc1;
   let acc2;
+  
+	const price = ethers.utils.parseUnits("1");
 
   this.beforeEach(async () => {
     // This is executed before each test
@@ -23,7 +25,7 @@ describe("AnimeCard", function () {
     expect(await myNFT.balanceOf(acc1.address)).to.equal(0);
 
     const tokenURI = "https://example.com/1";
-    const tx = await myNFT.connect(owner).safeMint(acc1.address, tokenURI);
+    const tx = await myNFT.connect(owner).uploadCardDetails(tokenURI, price);
     await tx.wait();
 
     expect(await myNFT.totalSupply()).to.equal(1);
@@ -33,7 +35,7 @@ describe("AnimeCard", function () {
   it("Should set the correct tokenURI of an anime card", async () => {
     const tokenURI_1 = "https://example.com/1";
 
-    const tx1 = await myNFT.connect(owner).safeMint(acc1.address, tokenURI_1);
+    const tx1 = await myNFT.connect(owner).uploadCardDetails(tokenURI_1, price);
     await tx1.wait();
 
     expect(await myNFT.tokenURI(0)).to.equal(tokenURI_1);
@@ -43,38 +45,30 @@ describe("AnimeCard", function () {
   it("Should mint an anime card to the correct owner", async () => {
     const tokenURI_1 = "https://example.com/1";
 
-    const tx1 = await myNFT.connect(owner).safeMint(acc1.address, tokenURI_1);
+    const tx1 = await myNFT.connect(owner).uploadCardDetails(tokenURI_1, price);
     await tx1.wait();
 
-    expect(await myNFT.ownerOf(0)).to.equal(acc1.address);
+    expect(await myNFT.ownerOf(0)).to.equal(owner.address);
   });
 
   // Fourth Test
   it("should upload and read a value from an anime card", async () => {
-    const image = "luffy.jpg";
-    const char_name = "Luffy";
-    const price = 2;
+    const tokenURI_1 = "https://example.com/1";
 
-    const upload = await myNFT.connect(owner).uploadCardDetails(char_name, image, price);
+    const upload = await myNFT.connect(owner).uploadCardDetails(tokenURI_1, price);
     await upload.wait();
 
     const readNFT = await myNFT.readAnimeCard(0);
-
-    expect(readNFT[1]).to.equal(char_name);
+    expect(readNFT[2]).to.equal(false);
+    expect(price.eq(readNFT[1])).to.equal(true);
   })
 
   // Fifth Test
   it("Should gift an anime card to another user", async () => {
-    const image = "luffy.jpg";
-    const char_name = "Luffy";
-    const price = 2;
     const tokenURI = "https://example.com/1";
 
-    const upload = await myNFT.connect(owner).uploadCardDetails(char_name, image, price);
+    const upload = await myNFT.connect(owner).uploadCardDetails(tokenURI, price);
     await upload.wait();
-
-    const tx = await myNFT.connect(owner).safeMint(owner.address, tokenURI);
-    await tx.wait();
 
     await myNFT.giftAnimeCard(0, acc2.address);
 
